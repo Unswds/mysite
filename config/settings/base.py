de0e64bf3fa361f82951293e1e3cc8cb219fd510
윤의진 <yuj121500@sasa.hs.kr>
2025-06-10 08:44:09 +0900

    image see

--- config/settings/base.py
+++ config/settings/base.py
@@ -125,6 +125,7 @@ USE_TZ        = True
 # Static & Media ---------------------------------------------------------
 STATIC_URL       = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL  = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
